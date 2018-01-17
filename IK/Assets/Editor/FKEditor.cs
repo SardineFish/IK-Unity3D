83632e96be2b2d56eacc8ccafa27558fc29326a4
SardineFish <Sardine@live.cn>
2018-01-17 23:44:35 +0800

    Implement Forward Kinematics

---
+++ IK/Assets/Editor/FKEditor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(FK))]
+    class FKEditor : UnityEditor.Editor
+    {
+        FK fk;
+        public void OnSceneGUI()
+        {
+            fk = target as FK;
+            if(fk.StartBone && fk.EndBone)
+            {
+                fk.Bones = SearchBones(fk.EndBone.transform, fk.StartBone).ToArray();
+            }
+        }
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+
+        public List<Bone> SearchBones(Transform current,Bone target)
+        {
+            if (current.GetComponent<Bone>() == target)
+                return new List<Bone>(new Bone[] { current.GetComponent<Bone>() });
+            if (!current.transform.parent)
+                throw new Exception("Cannot reach the Start Bone.");
+            var next = SearchBones(current.parent, target);
+            if (current.GetComponent<Bone>())
+                next.Add(current.GetComponent<Bone>());
+            return next;
+
+        }
+    }
+}
--- IK/Assets/Script/FK.cs
+++ IK/Assets/Script/FK.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class FK : MonoBehaviour {
     public Bone StartBone;
     public Bone EndBone;
+    public Bone[] Bones;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+    [ExecuteInEditMode]
 	// Update is called once per frame
 	void Update () {
-		
+        this.transform.position = EndBone.transform.position;
 	}
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(StartBone.transform.position, EndBone.transform.position);
+
+    }
+    private void OnDrawGizmos()
+    {
+        float axisLength = 3;
+        Color color;
+        ColorUtility.TryParseHtmlString("#66CCFF", out color);
+        Gizmos.color = color;
+        Gizmos.DrawLine(transform.position + Vector3.left * axisLength, transform.position + Vector3.right * axisLength);
+        Gizmos.DrawLine(transform.position + Vector3.forward * axisLength, transform.position + Vector3.back * axisLength);
+        Gizmos.DrawLine(transform.position + Vector3.up * axisLength, transform.position + Vector3.down * axisLength);
+    }
 }
